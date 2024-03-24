838dd98726a81b979d3b42549b6e9151d50527c1
AhsanWebdeve <ahsanrazaov@gamil.com>
2024-03-25 02:42:31 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Carousel from './Carousel';
+import Navbar from './Navbar';
+import Multikart from './Multikart';
+import Parralax from './Parralax';
+import Topnav from './Topnav';
+import Multi from './Multi';
+
+export default function App() {
+  return (
+    <>
+    <Topnav/>
+     <Navbar/>
+     <Carousel/>
+     <Multikart/>
+     <Parralax/>
+     <Multi/>
+    </>
+  )
+}
